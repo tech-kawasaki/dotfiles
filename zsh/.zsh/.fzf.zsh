6e47079490afc4eb5160a21e12d9e22cf4ba507b
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-03-28 16:12:54 +0900

    m1じゃなくてもfzfの補完が効くようにする

--- zsh/.zsh/.fzf.zsh
+++ zsh/.zsh/.fzf.zsh
@@ -12,4 +12,18 @@ if [[ $(arch) == arm64 ]]; then
   # Key bindings
   # ------------
   source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+elif [[ $(arch) == i386 ]]; then
+    # Setup fzf
+  # ---------
+  if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  # ------------
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 fi
