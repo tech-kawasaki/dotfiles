9862cf5cfb23903b6119f8d4c3be97c0725705bb
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-03-29 15:17:23 +0900

    微修正

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@
 # | iTerm2 |
 # +-------+
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
