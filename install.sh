56ef696e09ada52b30198ba6290ef50f3396fd05
Kawasaki Hirotsugu <36406175+tech-kawasaki@users.noreply.github.com>
2022-10-30 20:44:20 +0900

    modify ssh config

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
-# TODO: Throw error if ssh-key is't configured.
-ssh -T git@githu.com || {echo "Failed to access github.com. Please configure ssh key."; exit 1;}
+if [[ ! -d ${HOME}/.ssh ]]; then
+    echo "please configure ssh key"
+    exit 1
+fi
 
 # +------------------+
 # | Clone repository |
